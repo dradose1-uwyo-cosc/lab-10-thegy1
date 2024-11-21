71d2e179a8df35cb3f47eaf47dc6260d6761e87a
thegy1 <thegy@uwyo.edu>
2024-11-21 14:23:15 -0700

    Update and rename cosc1010-lab10.py to Hegy-COSC1010-lab10.py

--- Hegy-COSC1010-lab10.py
+++ Hegy-COSC1010-lab10.py
@@ -1,12 +1,10 @@
-# Your Name Here
+# Tyler Hegy
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# Submission Date: 11/21/2024
+# Lab 10
+# Lab Section: 12
+# Sources, people worked with, help given to: N/A
+# Comments: Wasn't sure what to do with the rockyou file when we first opened the txt file
 
 #import modules you will need 
 
@@ -22,13 +20,19 @@ def get_hash(to_hash):
 # Files and Exceptions
 
 # For this assignment, you will be writing a program to "crack" a password. You will need to open the file `hash` as this is the password you are trying to "crack."
-
 # To begin, you will need to open the 'rockyou.txt' file:
 # - This file contains a list of compromised passwords from the rockyou dump.
 # - This is an abridged version, as the full version is quite large.
 # - The file contains the plaintext version of the passwords. You will need to hash them to check against the password hash you are trying to crack.
 #   - You can use the provided `get_hash()` function to generate the hashes.
 #   - Be careful, as "hello" and "hello " would generate a different hash.
+try:
+    path1 = Path('rockyou.txt')
+    rock = path1.read_text()
+    hashed = get_hash(rock)
+except FileNotFoundError:
+    print('File Not Found')
+    hashed = False
 
 # You will need to include a try-except-catch block in your code.
 # - The reading of files needs to occur in the try blocks.
@@ -36,6 +40,12 @@ def get_hash(to_hash):
 
 # - Read in the value stored within `hash`.
 #   - You must use a try and except block.
+try:
+    path = Path('hash')
+    hash = path.read_text().strip()
+except FileNotFoundError:
+    print('File Not Found')
+    hash = False
 
 
 # Read in the passwords in `rockyou.txt`.
@@ -43,3 +53,19 @@ def get_hash(to_hash):
 # Hash each individual password and compare it against the stored hash.
 # - When you find the match, print the plaintext version of the password.
 # - End your loop.
+try:
+    path1 = Path('rockyou.txt')
+    rock = path1.read_text()
+    lines= rock.splitlines()
+except FileNotFoundError:
+    print('File Not Found')
+else:
+    for password in lines:
+        password = password.strip()
+        pass_hash = get_hash(password)
+        if pass_hash == hash:
+            print(f'Password is {password}')
+            break
+        else:
+            continue
+
